8a7699264e98ab3714e6e25ab71c42c301c8c584
chels-path <chelsbytheway7@gmail.com>
2026-03-02 09:16:43 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PlayersList from './PlayersList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
